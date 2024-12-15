e77461ea062f6dd6280da34f4121f8ef76bec400
Izayoi Sakuya <115458773+dotEntropy@users.noreply.github.com>
2024-12-15 02:36:25 -0800

    added keyframes

--- src/sprites/parents.py
+++ src/sprites/parents.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 from pygame.math import Vector2
 from src.loader import get_frames
 from src.variables import GameVars
@@ -30,59 +31,85 @@ class Animation(Graphics):
         Graphics.__init__(self)
         self.configs = {}
         self.config_id_fallback = config_id
-        self._update_config(config_id, asset_id, fps)
+        self._create_config(config_id, asset_id, fps)
         self._switch_config(config_id)
     
-    def _update_config(self, config_id: str, asset_id: str, fps: int, reset_idx: bool=True, loop: bool=True) -> None:
+    def _create_config(self, config_id: str, asset_id: str, fps: int, reset_idx: bool=True, loop: bool=True) -> None:
         frames = get_frames(asset_id)
         asset_id = frames['asset_id']
         self.configs.update({
             config_id: {
-            'asset_id': asset_id,
             'frames': frames,
             'total_frames': len(frames) - 1,
             'fps': fps,
             'reset_idx': reset_idx,
             'loop': loop
             }
-            })
+        })
     
-    def _switch_config(self, config_id: str) -> None:
+    def _switch_config(self, config_id: str, is_fallback: bool=False) -> None:
         if config_id not in self.configs:
             print(f'Config ID "{config_id}" does not exist!')
             return
-        config = self.configs[config_id] 
-        self.asset_id = config['asset_id']
-        self.frames = config['frames']
-        self.total_frames = config['total_frames']
-        self.fps = config['fps']
-        self.reset_idx = config['reset_idx']
-        self.loop = config['loop']
+        self.config_id = config_id
+        self.current_config = self.configs[config_id] 
+        self.asset_id = self.current_config['frames']['asset_id']
+        self.frames = self.current_config['frames']
+        self.total_frames = self.current_config['total_frames']
+        self.fps = self.current_config['fps']
+        self.reset_idx = self.current_config['reset_idx']
+        self.loop = self.current_config['loop']
 
         if self.reset_idx:
-            self.frame_idx_raw = 0
+            self.float_frame_idx = 0
+            self.int_frame_idx = 0
             self.pre_frame_idx = 0
-            self.frame_idx = 0
+            self.current_frame_idx = 0
 
         if self.loop:
             self.config_id_fallback = config_id
-
+        
+        if not is_fallback:
+            self._handle_keyframes()
         self._set_image()
     
     def _update_frame(self, dt: float) -> None:
-        self.frame_idx_raw = (self.frame_idx_raw + self.fps * dt) % self.total_frames
-        int_frame_idx = int(self.frame_idx_raw)
-        self.frame_idx = int_frame_idx
-
-        if self.frame_idx != self.pre_frame_idx:
-            self.pre_frame_idx = int_frame_idx
-            self._handle_single_loop()
-            self._set_image() 
+        self._update_frame_idx(dt)
+        if self.current_frame_idx != self.pre_frame_idx:
+            self.pre_frame_idx = self.int_frame_idx
+            self._handle_fallback()
+            self._handle_keyframes()
+            self._set_image()
     
-    def _handle_single_loop(self) -> None:
-        if not self.loop and self.frame_idx % self.total_frames == 0:
-            self._switch_config(self.config_id_fallback)
+    def _update_frame_idx(self, dt: float) -> None:
+        self.float_frame_idx = (self.float_frame_idx + self.fps * dt) % self.total_frames
+        self.int_frame_idx = int(self.float_frame_idx)
+        self.current_frame_idx = self.int_frame_idx
+    
+    
+    def _handle_fallback(self) -> None:
+        if not self.loop and self.current_frame_idx % self.total_frames == 0:
+            self._switch_config(self.config_id_fallback, is_fallback=True)
+        
+    def _handle_keyframes(self) -> None:
+        ...
     
     def _set_image(self) -> None:
-        if image := self.frames.get(f'{self.asset_id}-{self.frame_idx}'):
+        if image := self.frames.get(f'{self.asset_id}-{self.current_frame_idx}'):
             self._update_sprite(image)
+    
+    def _print_current_config(self) -> None:
+        cfg = {} 
+        cfg['frames'] = {}
+
+        for key in self.current_config:
+            if key == 'frames':
+                continue
+            value = str(self.current_config[key])
+            cfg.update({key: value})
+
+        for key in self.current_config['frames']:
+            value = str(self.current_config['frames'][key])
+            cfg['frames'].update({key: value})
+        
+        print(json.dumps(cfg, sort_keys=True, indent=4))
--- src/sprites/sakuya.py
+++ src/sprites/sakuya.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 from pygame.sprite import Sprite
 from pygame.math import Vector2
 from src.sprites.parents import Animation
@@ -8,21 +9,31 @@ from src.variables import GameVars
 class Sakuya(Sprite, Animation):
     def __init__(self):
         super().__init__()
-        Animation.__init__(self, config_id='default', asset_id='sakuya', fps=10)
-        self._update_config(
+        Animation.__init__(self, config_id='default', asset_id='sakuya', fps=8)
+        self._create_config(
             config_id='sakuya-attack', 
             asset_id='sakuya-attack', 
-            fps=12, 
+            fps=10, 
             loop=False
             )
     
     def _overrides(self):
-        self.pos = Vector2(GameVars.get_center_pos().x, GameVars.get_center_pos().y + 150)
-        self.scale = 3.0
+        self.pos = Vector2(GameVars.get_center_pos().x-550, GameVars.get_center_pos().y + 150)
+        self.scale = 2
+    
+    def _handle_keyframes(self):
+        if self.config_id == 'sakuya-attack' and self.current_frame_idx == 0:
+            self._print_current_config()
     
     def key_tap(self, key: int) -> None:
         if key == pygame.K_SPACE:
             self._switch_config('sakuya-attack')
+        if key == pygame.K_f:
+            self.pos.x = GameVars.get_center_pos().x-550
 
     def update(self, dt: float):
+        if self.config_id == 'default':
+            self.pos.x += 110 * dt * self.fps / 10
+            self.fps += 1 * dt
+            self.configs['default']['fps'] = self.fps
         self._update_frame(dt)
