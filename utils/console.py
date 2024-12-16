8a32d06dbd9c350e20e852c32a2fee427f9bafad
Izayoi Sakuya <115458773+dotEntropy@users.noreply.github.com>
2024-12-16 14:17:44 -0800

    removed console

--- utils/console.py
+++
@@ -1,9 +0,0 @@
-import colorama
-
-
-def print_error(text: str) -> None:
-    print(f'{colorama.Fore.RED}{text}{colorama.Style.RESET_ALL}')
-
-
-def print_success(text: str) -> None:
-    print(f'{colorama.Fore.GREEN}{text}{colorama.Style.RESET_ALL}')
