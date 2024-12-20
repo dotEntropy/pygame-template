d0cf139c1804f46c92a371de516c9cf79d79e09e
Izayoi Sakuya <115458773+dotEntropy@users.noreply.github.com>
2024-12-19 16:41:47 -0800

    updated button

--- src/states/parent.py
+++ src/states/parent.py
@@ -9,8 +9,9 @@ class State:
         is_reloaded = self.alias in GameVars.states
         GameVars.states[self.alias] = self
         self.SCREEN = pygame.display.get_surface()
-        self._init_groups()
+        self._init_buttons()
         self._init_sprites()
+        self._init_groups()
         load_str = 'reloaded!' if is_reloaded else 'loaded!'
         print(f'{colorama.Fore.GREEN}The state "{self.alias}" is {load_str}')
 
@@ -27,7 +28,8 @@ class State:
             state.__init__()
         else:
             print(f'{colorama.Fore.RED}State "{alias}" not found. Cannot reset.')
-    
+
+    def _init_buttons(self) -> None: ... 
     def _init_sprites(self) -> None: ...
     def _init_groups(self) -> None: ...
     def update(self, dt: float) -> None: ...
