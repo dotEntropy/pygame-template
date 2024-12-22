a25e132ac47ec3c50ec33ce4d10945c97e05c97d
Izayoi Sakuya <115458773+dotEntropy@users.noreply.github.com>
2024-12-21 17:54:13 -0800

    moved button to templates

--- src/states/default.py
+++ src/states/default.py
@@ -2,9 +2,9 @@ import pygame
 from pygame.math import Vector2
 from src.states.parent import State
 from utils.text import Text
+from src.sprites.templates import Button
 from src.variables import GameVars
 from src.sprites.stock_sprite import StockSprite
-from src.sprites.parents import Button
 
 
 class DefaultState(State):
